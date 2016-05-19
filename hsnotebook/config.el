a51c562e86909dbe31fa8803cccd5a13694a93f8
huangshuai <huangshuai010@gmail.com>
2016-05-20 06:17:28 +0800

    Add some configuration.
    - indent offset in web mode
    - tab width in nxml mode
    - shut down highlighting current line
    - config directory for private yasnippet snippets

--- hsnotebook/config.el
+++ hsnotebook/config.el
@@ -2,8 +2,19 @@
 (setq-default
  tab-width 4
  indent-tabs-mode t)
-(add-hook 'html-mode-hook (lambda ()
-							(setq tab-width 2)))
+
+(defun my-web-mode-hook ()
+  (setq tab-width 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(defun my-nxml-mode-hook ()
+  (setq tab-width 2))
+(add-hook 'nxml-mode-hook 'my-nxml-mode-hook)
+
+(add-to-list 'auto-mode-alist '(".xml") . emmet-mode)
 
 ;; search regex
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
@@ -12,3 +23,10 @@
 ;; evil key bindings
 (hs/define-key evil-normal-state-map
   "\\" 'evil-repeat-find-char-reverse)
+
+;; Don't highlight the current line
+(spacemacs/toggle-highlight-current-line-globally-off)
+
+;; Set default private snippets
+(setq auto-completion-private-snippets-directory
+	  (expand-file-name "snippet" dotspacemacs-directory))
