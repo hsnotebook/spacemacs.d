0f5b6113520b68d572f47f9da2d1636a99da5819
huangshuai <huangshuai010@gmail.com>
2016-06-13 23:43:31 +0800

    Fail to set tab-width to 2 in web mode and nxml mode.
    emmet-mode expands with two spaces. Can't fix that.
    Delete related configuration.

--- hsnotebook/config.el
+++ hsnotebook/config.el
@@ -3,19 +3,6 @@
  tab-width 4
  indent-tabs-mode t)
 
-(defun my-web-mode-hook ()
-  (setq tab-width 2)
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-(add-hook 'web-mode-hook 'my-web-mode-hook)
-
-(defun my-nxml-mode-hook ()
-  (setq tab-width 2))
-(add-hook 'nxml-mode-hook 'my-nxml-mode-hook)
-
-(add-to-list 'auto-mode-alist '(".xml" . emmet-mode))
-
 ;; search regex
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
